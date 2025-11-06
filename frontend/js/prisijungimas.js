317e280ea8b2f78f685357a68cebe5fce2337810
AugustasBartkus <augustas.bartkus@ktu.edu>
2025-11-07 00:54:32 +0200

    Prekes sarasas, Uzsakymu sarasas padaryti pilnai

---
+++ frontend/js/prisijungimas.js
@@ -0,0 +1,10 @@
+
+console.log("Prisijungimo langas įkeltas ✅");
+
+document.querySelector('.login').addEventListener('click', ()=>{
+  alert("Prisijungimas sėkmingas (placeholder).");
+});
+
+document.querySelector('.back').addEventListener('click', ()=>{
+  window.location.href = "/pagrindinis/";
+});
